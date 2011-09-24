d8af5c1c15b8758d143d4bbad8aae1d808cca074
Bruno Garcia <ah.duros@gmail.com>
2011-09-24 11:05:45 -0700

    Initial commit.

---
+++ src/ninja/ClientMain.hx
@@ -0,0 +1,16 @@
+//
+// Hanoi Hanger - A weekend HTML5 hack
+// https://github.com/aduros/hanoi/blob/master/LICENSE.txt
+
+package ninja;
+
+import flambe.System;
+
+class ClientMain
+{
+    private static function main ()
+    {
+        System.init();
+        trace("Hello world");
+    }
+}
